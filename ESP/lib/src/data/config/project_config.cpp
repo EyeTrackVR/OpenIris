ef1fa0314faf298b93b5f76e866db12059c62af7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-01 17:11:17 +0000

    update - found bug in config manager - fixed bug

--- ESP/lib/src/data/config/project_config.cpp
+++ ESP/lib/src/data/config/project_config.cpp
@@ -134,7 +134,7 @@ void ProjectConfig::load()
 	}
 
 	/* Device Config */
-	this->config.device.name = getString("deviceName", "openiris").c_str();
+	this->config.device.name = getString("deviceName", "openiristracker").c_str();
 	this->config.device.OTAPassword = getString("OTAPassword", "12345678").c_str();
 	this->config.device.OTAPort = getInt("OTAPort", 3232);
 	//! No need to load the JSON strings or bools, they are generated and used on the fly
