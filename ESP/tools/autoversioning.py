fd85d3c8ad835acc9f6789c19495cc234e9e4eaf
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-12-15 05:05:26 +0000

    update - include version.txt in repo - cleanup python code

--- ESP/tools/autoversioning.py
+++ ESP/tools/autoversioning.py
@@ -1,40 +1,28 @@
 FILENAME_VERSION = 'version.txt'  
 build_no = 'v0.0.0'
-
 try:
     with open(FILENAME_VERSION) as f:
         build_no = f.readline()
         if build_no == '':
             raise Exception("Empty file")
-        
         version = build_no.split('v')
         version = version[1].split('.')
-        ## check the last number in the version
         first_num = int(version[0])
         second_num = int(version[1])
         last_num = int(version[2])
-#
-        ## use sematic versioning to increment the version numbers and join them back together
         if last_num <= 9:
             last_num += 1
-            build_no = 'v' + str(first_num) + '.' + str(second_num) + '.' + str(last_num)
         elif second_num <= 9 and last_num >= 9:
             second_num += 1
-            build_no = 'v' + str(first_num) + '.' + str(second_num) + '.' + str(last_num)
         elif second_num and last_num >= 9:
-            # increment the first number and reset the second and last numbers
             first_num += 1
             second_num = 0
             last_num = 0
-            build_no = 'v' + str(first_num) + '.' + str(second_num) + '.' + str(last_num)
         else:
             raise Exception("Invalid version number")
+        build_no = 'v' + str(first_num) + '.' + str(second_num) + '.' + str(last_num)
 except:
     build_no = "v0.0.1"
-
-# create a file with the version number
 with open(FILENAME_VERSION, 'w+') as f:
     f.write(build_no)
-
-#print("-DVERSION=\"%s\"" % version+str(build_no))
 print("-DVERSION={0}".format(build_no))
