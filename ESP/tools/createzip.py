8d4d5cc1705628c38b0046201b4ff146f9ba9e20
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-31 12:01:29 +0000

    feat: testing createzip script

--- ESP/tools/createzip.py
+++ ESP/tools/createzip.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+Import("env")
 
 import sys
 from os import getcwd
@@ -6,25 +7,52 @@ from ntpath import basename
 from zipfile import ZipFile
 import json
 
+my_flags = env.ParseFlags(env["BUILD_FLAGS"])
+defines = dict()
+for x in my_flags.get("CPPDEFINES"):
+    if type(x) is tuple:
+        (k, v) = x
+        defines[k] = v
+        # print("Type Tuple: %s" % x)
+    elif type(x) is list:
+        k = x[0]
+        v = x[1]
+        defines[k] = v
+        # print("Type List: %s" % x)
+    else:
+        defines[x] = ""  # empty value
+        # print("Warning: unknown type for %s" % x)
+
+# strip quotes needed for shell escaping
+s = lambda x: x.replace('"', "")
+
+
 n = 2
 partitions_arg = sys.argv[1:]
-partitions = final = [partitions_arg[i * n:(i + 1) * n] for i in range((len(partitions_arg) + n - 1) // n )]
+partitions = final = [
+    partitions_arg[i * n : (i + 1) * n]
+    for i in range((len(partitions_arg) + n - 1) // n)
+]
 
-with ZipFile('build/openiris.zip', 'w') as archive:
-    print('Creating "' + archive.filename + '"', end='\n')
+# file_name = "build/" + str(env["PIOENV"]) + "/" + env["PROGNAME"] + ".zip"
+
+file_name = "build/{1}/{2}.zip".format(str(env["PIOENV"]), env["PROGNAME"])
+
+with ZipFile(file_name, "w") as archive:
+    print('Creating "' + archive.filename + '"', end="\n")
     parts = []
 
     for [offset, path] in partitions:
         filename = basename(path)
         archive.write(path, filename)
         partition = {
-            'path': filename,
-            'offset': int(offset, 16),
+            "path": filename,
+            "offset": int(offset, 16),
         }
         parts.append(partition)
 
     manifest = {
-        'chipFamily': 'ESP32',
-        'parts': parts,
+        "chipFamily": "ESP32",
+        "parts": parts,
     }
-    archive.writestr('manifest.json', json.dumps(manifest))
+    archive.writestr("manifest.json", json.dumps(manifest))
