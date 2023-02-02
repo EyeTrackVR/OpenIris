a2202d6288a76ab195d7cfb8fc14245d1a4715f9
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-02 10:14:07 +0000

    refactor: cleanup script

--- ESP/tools/createzip.py
+++ ESP/tools/createzip.py
@@ -8,35 +8,27 @@ import json
 
 
 def createZip(source, target, env):
-    if (
-        sys.platform.startswith("ubuntu")
-        or sys.platform.startswith("Linux")
-        or sys.platform.startswith("linux")
-    ):
+    if sys.platform.lower().startswith(("ubuntu", "linux")):
         print("Program has been built, creating zip archive!")
         program_path = target[0].get_abspath()
-        #print("binary path", program_path)
-        #print("FLASH_EXTRA_IMAGES: %s\n" % env["FLASH_EXTRA_IMAGES"])
-        #print("ESP32_APP_OFFSET: %s\n" % env["ESP32_APP_OFFSET"])
-        
+
         array_args = [env["FLASH_EXTRA_IMAGES"]]
 
-        for [offset, image] in env["FLASH_EXTRA_IMAGES"]:
+        for offset, image in env["FLASH_EXTRA_IMAGES"]:
             print("\nImage: %s" % str(image))
-            array_args.append(str(offset))
-            array_args.append(str(image))
+            array_args.extend([str(offset), str(image)])
 
         array_args.append(env["ESP32_APP_OFFSET"])
         array_args.append(program_path)
 
         n = 2
         partitions_arg = array_args[1:]
-        print("partitions_arg: %s\n" % partitions_arg)
+        print(f"partitions_args: {partitions_arg}")
         partitions = final = [
             partitions_arg[i * n : (i + 1) * n]
             for i in range((len(partitions_arg) + n - 1) // n)
         ]
-        print("partitions: %s\n" % partitions)
+        print(f"partitions: {partitions}")
         file_name = "./build/{0}/{1}.zip".format(str(env["PIOENV"]), env["PROGNAME"])
         with ZipFile(file_name, "w") as archive:
             print('\nCreating "' + archive.filename + '"', end="\n")
