92c8a659113274f757abab2f7635229503db8835
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-27 14:45:35 +0000

    ci(ci-binaries): testing merge

--- ESP/tools/createzip.py
+++ ESP/tools/createzip.py
@@ -59,6 +59,11 @@ def createZip(source, target, env):
                 version = str(defines.get("PIO_SRC_TAG"))
                 new_install_prompt_erase = True
 
+
+                print("Creating manifest.json")
+
+                print("[PARTITIONS]: {}".format(partitions))
+
                 """
                 python esptool.py --chip ESP32 merge_bin -o merged-firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
                 0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot.bin 0x10000 OpenIris-v1.3.0-esp32AIThinker-8229a3a-master.bin
