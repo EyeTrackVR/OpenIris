b6e7172300231165002ce7549335197cd10e0e7c
lorow <smykupyka@gmail.com>
2023-04-06 01:05:24 +0200

    Add very basic async client for OpenIris API

---
+++ PythonTools/OpenIrisClient.py
@@ -0,0 +1,151 @@
+from typing import Optional, Callable
+
+import aiohttp
+
+from PythonTools.models import (
+    DeviceConfig,
+    CameraConfig,
+    WiFiConfig,
+    TrackerConfig,
+    WiFiTXPower,
+    MDNSConfig,
+)
+
+
+class BaseAPIClient:
+    def __init__(self, tracker_address: str):
+        self.tracker_address = tracker_address
+        self.base_endpoint = "control/builtin/command"
+        self.session = aiohttp.ClientSession()
+
+    async def get(
+        self,
+        command: str,
+        params: Optional[dict] = None,
+        validator: Optional[Callable] = None,
+    ):
+        clean_params = await self._clean_params(params)
+
+        if validator and not validator(clean_params):
+            raise ValueError(
+                f"Params for command {command} are required, none provided"
+            )
+
+        async with self.session.get(
+            f"{self.tracker_address}/{self.base_endpoint}/{command}/",
+            params=clean_params,
+        ) as request:
+            return request
+
+    async def post(
+        self, command: str, data: dict, validator: Optional[Callable] = None
+    ):
+        clean_params = await self._clean_params(data)
+
+        if validator and not validator(clean_params):
+            raise ValueError(
+                f"Params for command {command} are required, none provided"
+            )
+
+        async with self.session.post(
+            f"{self.tracker_address}/{self.base_endpoint}/{command}/",
+            params=clean_params,
+        ) as request:
+            return request
+
+    async def delete(
+        self, command: str, data: dict, validator: Optional[Callable] = None
+    ):
+        clean_params = await self._clean_params(data)
+
+        if validator and not validator(clean_params):
+            raise ValueError(
+                f"Params for command {command} are required, none provided"
+            )
+
+        async with self.session.delete(
+            f"{self.tracker_address}/{self.base_endpoint}/{command}/",
+            params=clean_params,
+        ) as request:
+            return request
+
+    @staticmethod
+    async def _clean_params(params) -> dict:
+        if params:
+            return {k: v for k, v in params.items() if v not in (None, "")}
+        return {}
+
+
+class OpenIrisClient(BaseAPIClient):
+    async def upsert_wifi_settings(self, network: WiFiConfig):
+
+        params = {
+            "networkName": network.name,
+            "ssid": network.ssid,
+            "password": network.password,
+            "channel": network.channel,
+            "power": network.power,
+            "adhoc": network.adhoc,
+        }
+
+        return await self.post("setWiFi", data=params, validator=all)
+
+    async def remove_wifi_network(self, name: str):
+        params = {
+            "networkName": name,
+        }
+
+        return await self.delete("setWiFi", data=params, validator=all)
+
+    async def get_stored_config(self) -> TrackerConfig:
+        response = await self.get("getStoredConfig")
+        return TrackerConfig(**await response.json())
+
+    async def reset_config(self):
+        return await self.get("resetConfig")
+
+    # we should split this into two separate configs and endpoints and clean them up
+    async def update_device_settings(
+        self, mdns_config: MDNSConfig, device_config: DeviceConfig
+    ):
+        params = {
+            "hostname": mdns_config.hostname,
+            "service": mdns_config.service,
+            "ota_login": device_config.OTALogin,
+            "ota_password": device_config.OTAPassword,
+        }
+
+        return await self.get(command="setDevice", params=params)
+
+    async def reboot_device(self):
+        return await self.get(command="rebootDevice")
+
+    async def set_tx_power(self, power_level: WiFiTXPower):
+        params = {
+            "txPower": power_level.power,
+        }
+        return await self.get(command="setTxPower", params=params)
+
+    async def get_wifi_strength(self):
+        return await self.get(command="wifiStrength")
+
+    async def update_camera_settings(self, camera_config: CameraConfig):
+        params = {
+            "framesize": camera_config.framesize,
+            "vflip": camera_config.vflip,
+            "hflip": camera_config.hflip,
+            "quality": camera_config.quality,
+            "brightness": camera_config.brightness,
+        }
+
+        return await self.get(command="setCamera", params=params)
+
+    async def restart_camera(self, hard_reset: bool = False):
+        params = {"mode": hard_reset}
+        return await self.get(command="restartCamera", params=params)
+
+    async def ping(self):
+        return await self.get(command="ping")
+
+    async def save_config(self):
+        return await self.get(command="save")
---
+++ PythonTools/models.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+from PythonTools.constants import FrameSize, WifiPowerPoint
+
+
+class DeviceConfig(BaseModel):
+    OTALogin: Optional[str] = None
+    OTAPassword: Optional[str] = None
+
+
+class MDNSConfig(BaseModel):
+    hostname: Optional[str] = None
+    service: Optional[str] = None
+
+
+class CameraConfig(BaseModel):
+    framesize: Optional[FrameSize] = None
+    vflip: Optional[bool] = None
+    hflip: Optional[bool] = None
+    quality: Optional[int] = None
+    brightness: Optional[int] = None
+
+
+class WiFiConfig(BaseModel):
+    name: str
+    ssid: str
+    password: str
+    channel: int
+    power: int
+    adhoc: bool
+
+
+class WiFiTXPower(BaseModel):
+    power: WifiPowerPoint
+
+
+class APWiFiConfig(BaseModel):
+    ssid: str
+    password: str
+    channel: int
+    adhoc: bool
+
+
+class TrackerConfig(BaseModel):
+    device_config: DeviceConfig
+    camera_config: CameraConfig
+    networks: List[WiFiConfig]
+    AP_WiFi_config: APWiFiConfig
+    mdns_config: MDNSConfig
+    Wifi_TX_power: WiFiTXPower
