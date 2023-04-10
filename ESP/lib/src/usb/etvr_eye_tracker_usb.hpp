8e34e31a2ae9ceed31c2e0c222124b4029b0c130
Zdzislaw Goik <smykupyka@gmail.com>
2023-04-10 20:36:12 +0200

    feat: usb support for esp32s3 (#47)
    * fix: completed esp32s3 boards config
    
    * feat: enable usb cdc on boot
    * fix: memory_type has to be octal qio_opi for psram to init
    * fix: cannot flash from platformio without --no-stub flag
    
    * fix: fixed pinouts copy paste issues
    
    * feat: add system header to unify future traits
    
    * feat: add rudimentary usb cdc video streaming impl
    
    * feat: add rudimentary impl for usb tracker in main
    
    * fix: revert wrovers3 board names
    
    * feat: disable radio to reduce power usage
    
    * feat: add v2 serial comms with packet headers
    
    ---------
    
    authored-by: Sebastian Fitt <sebastian.fitt@sebastianfitt.pl>

---
+++ ESP/lib/src/usb/etvr_eye_tracker_usb.hpp
@@ -0,0 +1,7 @@
+#ifndef INCLUDED_ETVR_EYE_TRACKER_USB_HPP
+#define INCLUDED_ETVR_EYE_TRACKER_USB_HPP
+
+void etvr_eye_tracker_usb_init();
+void etvr_eye_tracker_usb_loop();
+
+#endif  // INCLUDED_ETVR_EYE_TRACKER_USB_HPP
