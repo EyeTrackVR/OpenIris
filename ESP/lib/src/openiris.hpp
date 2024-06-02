47b52417d0c61fe1a0444d784307001897212f62
Lorow <goik.zdzislaw@gmail.com>
2024-06-02 15:35:53 +0200

    remove unused etvr_eye_tracker_usb

--- ESP/lib/src/openiris.hpp
+++ ESP/lib/src/openiris.hpp
@@ -3,20 +3,18 @@
 
 #include <Arduino.h>
 
+#include <data/CommandManager/CommandManager.hpp>
 #include <data/config/project_config.hpp>
 #include <io/LEDManager/LEDManager.hpp>
+#include <io/Serial/SerialManager.hpp>
 #include <io/camera/cameraHandler.hpp>
 #include <logo/logo.hpp>
-#include <io/Serial/SerialManager.hpp>
-#include <data/CommandManager/CommandManager.hpp>
 
 #ifndef ETVR_EYE_TRACKER_USB_API
 #include <network/api/webserverHandler.hpp>
 #include <network/mDNS/MDNSManager.hpp>
 #include <network/stream/streamServer.hpp>
 #include <network/wifihandler/wifihandler.hpp>
-#else
-#include <usb/etvr_eye_tracker_usb.hpp>
 #endif  // ETVR_EYE_TRACKER_WEB_API
 
 #endif  // OPENIRIS_HPP
--- ESP/lib/src/usb/etvr_eye_tracker_usb.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef INCLUDED_ETVR_EYE_TRACKER_USB_HPP
-#define INCLUDED_ETVR_EYE_TRACKER_USB_HPP
-
-void etvr_eye_tracker_usb_init();
-void etvr_eye_tracker_usb_loop();
-
-#endif  // INCLUDED_ETVR_EYE_TRACKER_USB_HPP
